376257e35ff1f7c23825063a959f60a045a71719
Vitor Vaz <vitor.vaz.andrade@gmail.com>
2023-10-16 14:08:05 -0300

    remove coments

--- config/config.exs
+++ config/config.exs
@@ -1,16 +1,8 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-
-# General application configuration
 use Mix.Config
 
 config :obkap,
   ecto_repos: [Obkap.Repo]
 
-# Configures the endpoint
 config :obkap, ObkapWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "ymAD2amVtnFoPVkHLdBAuHxzYaX2RZnMx7Wsec+RpNbdKpz2qNjolsqb+WFrvIO4",
@@ -18,14 +10,10 @@ config :obkap, ObkapWeb.Endpoint,
   pubsub_server: Obkap.PubSub,
   live_view: [signing_salt: "POg5mDW7"]
 
-# Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
--- config/test.exs
+++ config/test.exs
@@ -1,22 +1,14 @@
-use Mix.Config
+import Config
 
-# Configure your database
-#
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
 config :obkap, Obkap.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "obkap_user",
+  password: "obkap_password",
   database: "obkap_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :obkap, ObkapWeb.Endpoint,
   http: [port: 4002],
   server: false
 
-# Print only warnings and errors during test
 config :logger, level: :warn
